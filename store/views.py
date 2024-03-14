19b409263cd1bff86acf8c035781700f49386996
jruribem10 <jruribem@eafit.edu.co>
2024-03-14 13:35:30 -0500

    Carrito

--- store/views.py
+++ store/views.py
@@ -1,12 +1,54 @@
 from django.shortcuts import render,redirect
-from.models import Product,Category
+from.models import Product,Category,Profile
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .forms import SignUpForm,UpdateUserForm,ChangePasswordForm
+from .forms import SignUpForm,UpdateUserForm,ChangePasswordForm,UserInfoForm
 from django import forms
 # Create your views here.
+
+
+def search(request):
+	# Determine if they filled out the form
+	if request.method == "POST":
+		searched = request.POST['searched']
+		# Query The Products DB Model
+		searched = Product.objects.filter(name__icontains=searched)
+		# Test for null
+		if not searched:
+			messages.success(request, "That Product Does Not Exist...Please try Again.")
+			return render(request, "search.html", {})
+		else:
+			return render(request, "search.html", {'searched':searched})
+	else:
+		return render(request, "search.html", {})	
+
+
+
+def update_info(request):
+	if request.user.is_authenticated:
+		# Get Current User
+		current_user = Profile.objects.get(user__id=request.user.id)
+		# Get Current User's Shipping Info
+		#shipping_user = ShippingAddress.objects.get(user__id=request.user.id)
+		
+		# Get original User Form
+		form = UserInfoForm(request.POST or None, instance=current_user)
+		# Get User's Shipping Form
+		#shipping_form = ShippingForm(request.POST or None, instance=shipping_user)		
+		if form.is_valid(): #or shipping_form.is_valid():
+			# Save original form
+			form.save()
+			# Save shipping form
+			#shipping_form.save()
+
+			messages.success(request, "Your Info Has Been Updated!!")
+			return redirect('home')
+		return render(request, "update_info.html", {})
+	else:
+		messages.success(request, "You Must Be Logged In To Access That Page!!")
+		return redirect('home')
 
 
 def update_password(request):
@@ -119,7 +161,7 @@ def register_user(request):
             user=authenticate(username=username, password=password)
             login(request,user)
             messages.success(request, (" Ha sido registrado con extio"))
-            return redirect('home')
+            return redirect('update_info')
 
     else:
         return render(request, 'register.html',{'form':form} )
